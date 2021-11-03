9bf1e048871e44b8b141fff741eceeb0d26d6e2d
Jose <jose.renato@al.infnet.edu.br>
2021-11-03 12:12:19 -0300

    loja-pet-store

---
+++ pet/parte1/js/script.js
@@ -0,0 +1,153 @@
+ 
+
+// #####1
+
+// const arr = [];
+// for (let i = 0; i < 101; i++){
+// if(i % 7 == 0){
+//   arr.push(i);
+// }
+// }
+// console.log(arr);
+
+
+
+
+// ####### 2
+// let text = prompt("Digite um texto: ");
+// let qntExib = prompt("Digite a quantidade de exibições deste texto:")
+
+// const ex2 = (texto,vezes) => {
+//   let x = 1;
+// while(x <= vezes){
+//   console.log(texto +" , "+ x);
+//   x++;
+// }
+// }
+// ex2(text,qntExib);
+
+
+
+// ##### 3 
+
+// let node = document.querySelector(".filho")
+// document.querySelector(".pai").appendChild(node);
+
+
+// ###4
+
+// let element = document.getElementById("divv");
+//    element.classList.add("textos");
+
+//   document.querySelector(".textos").children[1].innerHTML = "Texto alterado";
+
+//   let ultimo = document.getElementById("divv").children.length - 1;
+
+//   let element2 = document.getElementById("divv").children[ultimo];
+//    element2.classList.add("last");
+
+// ###5
+
+// const recebeIdade = (idade) => {
+// idade >= 18 ? console.log("Maior de Idade!") : console.log("Menor de Idade")
+// }
+// recebeIdade(17);
+
+// ### 6 
+
+// let obj = {
+//   nomeObj : 'Mouse',
+//   pesoObj: '150g'
+//   }
+
+//   const newObj = (Objeto) => {
+//     let arr = [];
+//    const novoObjeto = {
+//      ...Objeto,
+//      nomeObj: "Teclado",
+//      pesoObj: "500g"
+//    }
+
+//    arr.push(novoObjeto);
+//    console.log(arr)
+//   }
+
+//   newObj(obj)
+
+
+
+// ###7 
+
+
+const texto = "Eu quero trabalhar na Seri.e Design";
+//a
+// let novoTexto = texto.replaceAll(" ", "!");
+
+// function reverse(text){
+//   return text.split(" ").reverse().join("!");
+// }
+// reverse(texto);
+// console.log(reverse(texto));
+
+
+/* b
+ */
+// let palavras = texto.split(' ');
+// let ultimo = palavras[palavras.length - 1];
+
+// console.log(ultimo);
+
+/* c */
+// console.log(`Caracteres do texto: ${texto.length}`);
+
+// /* d
+//  */
+// console.log(typeof texto)
+// console.log(typeof palavras)
+
+// ####8
+
+  
+  
+//   let mydivs = document.getElementsByTagName("div");
+
+//   for(let i = 0; i <= mydivs.length; i++){
+
+//     mydivs[i].setAttribute("data-id",`${i+1}`)
+
+//     console.log( mydivs[i])
+//   }
+// console.log(mydivs.length)
+
+
+// ##9
+
+  
+// function clickCat() {
+//     // document.querySelector("#ul-subcategory").style.display = "block";
+
+//     document.querySelector(".subcontainer").children[0].style.display = "block";
+    
+
+//   }
+//   var element = document.querySelector('.container');
+//   var element2 = document.querySelector('.subcontainer');
+//     element.classList.add('active');
+//     element2.classList.add('active');
+
+//     console.log(document.querySelector('.container'))
+//     console.log(document.querySelector('.subcontainer'))
+
+
+
+// #10
+
+
+  
+  // #10
+
+//Ao executar o código o contador não para. Então acredito que A)
+
+
+  
+  
